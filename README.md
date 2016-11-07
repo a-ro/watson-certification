4bedb9c6cbb8746dac8fc3086c06d559142eb195
Amelie <amelie.rolland.1@gmail.com>
2016-11-07 10:28:03 -0500

    add tone analyzer example

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 Study Guide for Watson Certification
 
 I made this guide to study before the Watson certification exam. I'm following this [guide](http://public.dhe.ibm.com/partnerworld/pub/certify/Study_Guide_C7020_230.pdf)  realeased by IBM which details the exam objectives. For each objective, I make my own answers and give some code examples when required. I wrote these answers in the markdown file of each section. Note that these answers are my own and have not been validated by IBM.
+
+# Sections
+- [Section 1 - Fundamentals of Cognitive Computing](https://github.com/a-ro/watson-certification/blob/master/section1/section1.md)
+
+# Examples
+## Tone Analyzer
+Replace username and password with your credentials in [tone_analyzer_example.py](https://github.com/a-ro/watson-certification/blob/master/section3/tone/tone_analyzer_example).
+Open a terminal. Go in the root of the project. Run the example with:
+``` shell
+python section3/tone/tone_analyzer_example.py
+```
---
+++ section3/tone/tone_analyzer_example.py
@@ -0,0 +1,45 @@
+import os
+
+import pandas
+from watson_developer_cloud import ToneAnalyzerV3
+
+
+def tone_analyzer_example():
+    tone_analyzer = ToneAnalyzerV3(username='username', password='password',
+                                   version='2016-05-19')
+    text = __read_text()
+    document_tone = tone_analyzer.tone(text=text)
+    __save_to_csv(document_tone)
+
+
+def __read_text():
+    file_path = __get_file_path('angry-text.txt')
+    with open(file_path, 'r') as text_file:
+        text = text_file.read()
+    print('Text to analyze:')
+    print(text)
+    print()
+    return text
+
+
+def __save_to_csv(document_tone):
+    formatted_sentence_tones = []
+    for sentence_tone in document_tone['sentences_tone']:
+        current_tones = {'sentence': sentence_tone['text']}
+        for tone_category in sentence_tone['tone_categories']:
+            category = tone_category['category_name']
+            tones = tone_category['tones']
+            category_tones = {'{}:{}'.format(category, tone['tone_name']): tone['score'] for tone in tones}
+            current_tones.update(category_tones)
+        formatted_sentence_tones.append(current_tones)
+    dataframe = pandas.DataFrame(formatted_sentence_tones)
+    dataframe.to_csv(__get_file_path('analyzed-tone.csv'))
+    print('Results were saved in analyzed-tone.csv')
+
+
+def __get_file_path(file_name):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name)
+
+
+if __name__ == '__main__':
+    tone_analyzer_example()
